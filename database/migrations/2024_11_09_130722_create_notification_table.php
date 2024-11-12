6284177058c8f919b8a0e261f2bf23ac20de00b5
Abduvaxobova <146168487+Abduvaxobova@users.noreply.github.com>
2024-11-12 18:23:56 +0500

    I created migrations for save to database table notifications

---
+++ database/migrations/2024_11_09_130722_create_notification_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('notifiable_id');
+            $table->string('notifiable_type');
+            $table->string('type');
+            $table->text('data');
+            $table->timestamp('read_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
